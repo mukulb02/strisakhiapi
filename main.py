d3389d655f975fe0998228adc2f66d288e0f64ec
Mukul Bedwa <39831692+mukulb02@users.noreply.github.com>
2024-07-27 16:37:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import os
 
 # Define the response model
 class QueryResponse(BaseModel):
@@ -14,7 +15,7 @@ tokenizer_name = "PY007/TinyLlama-1.1B-Chat-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 
 # Load the model directly without quantization settings
-model_path = "PY007/TinyLlama-1.1B-Chat-v0.1"
+model_path = "C:/Users/IIITU/Desktop/tinyllama-strisakhi"
 
 # Load the model without any quantization configuration
 model = AutoModelForCausalLM.from_pretrained(model_path, revision='main', use_safetensors=False)
@@ -23,14 +24,15 @@ model.to("cpu")
 def generate_data(query, model, tokenizer):
     inputs = tokenizer(query, return_tensors="pt")
     generation_config = {
-        "max_length": 250,
+        "max_new_tokens": 250,
         "pad_token_id": tokenizer.eos_token_id,
         "repetition_penalty": 1.2,
         "temperature": 0.7,
         "top_p": 0.9,
-        "eos_token_id": tokenizer.eos_token_id
+        "eos_token_id": tokenizer.eos_token_id,
+        "do_sample": True  # Ensure sampling-based generation
     }
-    outputs = model.generate(input_ids=inputs.input_ids, **generation_config)
+    outputs = model.generate(input_ids=inputs.input_ids, attention_mask=inputs.attention_mask, **generation_config)
     text_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return text_output[len(query):].strip()
 
@@ -45,4 +47,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
